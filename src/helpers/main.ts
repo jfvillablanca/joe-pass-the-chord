29f58ed994290247c074f50babdc7a08bb4651cc
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-01 17:56:02 +0800

    Feat: Add Note object literals

--- src/helpers/main.ts
+++ src/helpers/main.ts
@@ -67,3 +67,129 @@ type Note = {
     intervalFromCNatural: NoteIntervalFromCNatural;
     relativeOctave?: number;
 };
+
+const C: Note = {
+    name: "C",
+    spelling: "C♮",
+    intervalFromCNatural: 0,
+};
+
+const CSharp: Note = {
+    name: "C sharp",
+    spelling: "C♯",
+    intervalFromCNatural: 1,
+};
+
+const CFlat: Note = {
+    name: "C flat",
+    spelling: "C♭",
+    intervalFromCNatural: -1,
+};
+
+const D: Note = {
+    name: "D",
+    spelling: "D♮",
+    intervalFromCNatural: 2,
+};
+
+const DSharp: Note = {
+    name: "D sharp",
+    spelling: "D♯",
+    intervalFromCNatural: 3,
+};
+
+const DFlat: Note = {
+    name: "D flat",
+    spelling: "D♭",
+    intervalFromCNatural: 1,
+};
+
+const E: Note = {
+    name: "E",
+    spelling: "E♮",
+    intervalFromCNatural: 4,
+};
+
+const ESharp: Note = {
+    name: "E sharp",
+    spelling: "E♯",
+    intervalFromCNatural: 5,
+};
+
+const EFlat: Note = {
+    name: "E flat",
+    spelling: "E♭",
+    intervalFromCNatural: 3,
+};
+
+const F: Note = {
+    name: "F",
+    spelling: "F♮",
+    intervalFromCNatural: 5,
+};
+
+const FSharp: Note = {
+    name: "F sharp",
+    spelling: "F♯",
+    intervalFromCNatural: 6,
+};
+
+const FFlat: Note = {
+    name: "F flat",
+    spelling: "F♭",
+    intervalFromCNatural: 4,
+};
+
+const G: Note = {
+    name: "G",
+    spelling: "G♮",
+    intervalFromCNatural: 7,
+};
+
+const GSharp: Note = {
+    name: "G sharp",
+    spelling: "G♯",
+    intervalFromCNatural: 8,
+};
+
+const GFlat: Note = {
+    name: "G flat",
+    spelling: "G♭",
+    intervalFromCNatural: 6,
+};
+
+const A: Note = {
+    name: "A",
+    spelling: "A♮",
+    intervalFromCNatural: 9,
+};
+
+const ASharp: Note = {
+    name: "A sharp",
+    spelling: "A♯",
+    intervalFromCNatural: 10,
+};
+
+const AFlat: Note = {
+    name: "A flat",
+    spelling: "A♭",
+    intervalFromCNatural: 8,
+};
+
+const B: Note = {
+    name: "B",
+    spelling: "B♮",
+    intervalFromCNatural: 11,
+};
+
+const BSharp: Note = {
+    name: "B sharp",
+    spelling: "B♯",
+    intervalFromCNatural: 12,
+};
+
+const BFlat: Note = {
+    name: "B flat",
+    spelling: "B♭",
+    intervalFromCNatural: 10,
+};
