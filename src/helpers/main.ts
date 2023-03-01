c2d21228639e0890bc6c5fab8fd597a50c338633
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-01 17:59:12 +0800

    Feat: add Interval types

--- src/helpers/main.ts
+++ src/helpers/main.ts
@@ -217,3 +217,38 @@ const Notes = {
     BSharp,
     BFlat,
 };
+
+type QualityName =
+    | "Perfect unison"
+    | "Minor second"
+    | "Major second"
+    | "Minor third"
+    | "Major third"
+    | "Perfect fourth"
+    | "Tritone"
+    | "Perfect fifth"
+    | "Minor sixth"
+    | "Major sixth"
+    | "Minor seventh"
+    | "Major seventh"
+    | "Perfect octave";
+
+type QualityNameShort =
+    | "P1"
+    | "m2"
+    | "M2"
+    | "m3"
+    | "M3"
+    | "P4"
+    | "TT"
+    | "P5"
+    | "m6"
+    | "M6"
+    | "m7"
+    | "M7"
+    | "P8";
+
+type Interval = {
+    semitoneDistance: number;
+    quality: { name: QualityName; shortName?: QualityNameShort };
+};
