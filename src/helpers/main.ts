6948c54adef9de75f1604aa84076893e08e6332d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-01 12:20:37 +0800

    Draft: object literal implementation of a Note

---
+++ src/helpers/main.ts
@@ -0,0 +1,15 @@
+const normalizedNote = {
+    An: ["A♮"],
+    AB: ["A♯","B♭"],
+    Bn: ["B♮"],
+    Cn: ["C♮"],
+    CD: ["C♯","D♭"],
+    Dn: ["D♮"],
+    DE: ["D♯","E♭"],
+    En: ["E♮"],
+    Fn: ["F♮"],
+    FG: ["F♯","G♭"],
+    Gn: ["G♮"],
+    GA: ["G♯","A♭"],
+}
+export {  };
