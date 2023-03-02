31e9c61c39f12ebbe883ee685299a53dfe39a259
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-02 12:17:49 +0800

    Test: add ascending interval test

--- src/tests/App.test.ts
+++ src/tests/App.test.ts
@@ -1,8 +1,17 @@
-import { describe, test } from "vitest";
+import { describe, test, expect } from "vitest";
+import { getNoteFromInterval, Notes, Intervals } from "../helpers/main";
 
-describe.todo("Interval tests", () => {
+describe("Interval tests", () => {
+    test("returns the correct note in ascending interval", () => {
+        const targetNote = getNoteFromInterval(
+            Notes.C,
+            Intervals.majorSixth,
+            "ascending"
+        );
 
-})
+        expect(targetNote).toBe(Notes.A);
+    });
+});
 
 describe.todo("Scale tests", () => {
 
