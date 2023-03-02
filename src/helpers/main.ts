bb9f37a37e9e459089351f569c136212c925c9c7
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-02 10:16:30 +0800

    Feat: add getNoteFromInterval()

--- src/helpers/main.ts
+++ src/helpers/main.ts
@@ -2,3 +2,29 @@ import { Notes } from "./notes";
 import { Intervals } from "./intervals";
 
 export { Notes, Intervals };
+function getNoteFromInterval(
+    refNote: Note,
+    interval: Interval,
+    direction: "ascending" | "descending"
+): Note {
+    const absInterval = refNote.intervalFromCNatural;
+    const relativeInterval = interval.semitoneDistance;
+
+    const [intervalNote]: Note[] = Object.values(Notes).filter((note: Note) => {
+        if (
+            direction === "ascending" &&
+            note.intervalFromCNatural === absInterval + relativeInterval
+        ) {
+            return note;
+        }
+
+        if (
+            direction === "descending" &&
+            note.intervalFromCNatural === absInterval - relativeInterval
+        ) {
+            return note;
+        }
+    });
+
+    return intervalNote;
+}
