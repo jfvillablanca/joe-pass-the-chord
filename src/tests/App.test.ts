62040b76490bf3250de2df4adb8d613719d2f2e9
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-02 13:29:30 +0800

    Test: add another expect assertion to ascend test

--- src/tests/App.test.ts
+++ src/tests/App.test.ts
@@ -2,12 +2,18 @@ import { describe, test, expect } from "vitest";
 import { getNoteFromInterval, Notes, Intervals } from "../helpers/main";
 
 describe("Interval tests", () => {
-
     test("returns the correct note when interval is ascending", () => {
-
         expect(
             getNoteFromInterval(Notes.C, Intervals.majorSixth, "ascending")
         ).toBe(Notes.A);
+
+        expect(
+            getNoteFromInterval(
+                Notes.ASharp,
+                Intervals.majorSecond,
+                "ascending"
+            )
+        ).toBe(Notes.BSharp);
 
     });
 
