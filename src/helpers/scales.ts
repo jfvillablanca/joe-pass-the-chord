3f47e67b9ddbebae53898b55001eb6f465826a3a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-03 13:44:40 +0800

    Feat: add Phrygian up to Locrian to Scale

--- src/helpers/scales.ts
+++ src/helpers/scales.ts
@@ -21,8 +21,64 @@ const Dorian: Interval[] = [
     Intervals.minorSeventh,
 ];
 
+const Phrygian: Interval[] = [
+    Intervals.perfectUnison,
+    Intervals.minorSecond,
+    Intervals.minorThird,
+    Intervals.perfectFourth,
+    Intervals.perfectFifth,
+    Intervals.minorSixth,
+    Intervals.minorSeventh,
+];
+
+const Lydian: Interval[] = [
+    Intervals.perfectUnison,
+    Intervals.majorSecond,
+    Intervals.majorThird,
+    Intervals.tritoneAugmented,
+    Intervals.perfectFifth,
+    Intervals.majorSixth,
+    Intervals.majorSeventh,
+];
+
+const Mixolydian: Interval[] = [
+    Intervals.perfectUnison,
+    Intervals.majorSecond,
+    Intervals.majorThird,
+    Intervals.perfectFourth,
+    Intervals.perfectFifth,
+    Intervals.majorSixth,
+    Intervals.minorSeventh,
+];
+
+const Aeolian: Interval[] = [
+    Intervals.perfectUnison,
+    Intervals.majorSecond,
+    Intervals.minorThird,
+    Intervals.perfectFourth,
+    Intervals.perfectFifth,
+    Intervals.minorSixth,
+    Intervals.minorSeventh,
+];
+
+const Locrian: Interval[] = [
+    Intervals.perfectUnison,
+    Intervals.minorSecond,
+    Intervals.minorThird,
+    Intervals.perfectFourth,
+    Intervals.tritoneDiminished,
+    Intervals.minorSixth,
+    Intervals.minorSeventh,
+];
+
 const Scale = {
     Ionian,
     Dorian,
+    Phrygian,
+    Lydian,
+    Mixolydian,
+    Aeolian,
+    Locrian,
 };
+
 export { Scale };
