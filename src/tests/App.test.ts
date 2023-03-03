ddcabce5d17117ecd4c47647352e597446ac55b5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-03 11:35:05 +0800

    Test: add test for F# Ionian mode

--- src/tests/App.test.ts
+++ src/tests/App.test.ts
@@ -75,4 +75,17 @@ describe("Scale tests", () => {
         expect(getScaleTones(N.C, S.Ionian)).toStrictEqual(ionianMode);
     });
 
+    test("returns the notes of the Ionian mode in the key of F#", () => {
+        const ionianMode = [
+            N.FSharp,
+            N.GSharp,
+            N.ASharp,
+            N.B,
+            N.CSharp,
+            N.DSharp,
+            N.ESharp,
+        ];
+        expect(getScaleTones(N.FSharp, S.Ionian)).toStrictEqual(ionianMode);
+    });
+
 });
