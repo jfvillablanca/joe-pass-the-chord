e1d3b869b01995881e3ece1cb541704a585ae50c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-06 17:44:06 +0800

    Feat: render fretboard to DOM

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,7 +1,21 @@
-import { useFretClickContext } from "./MainContext";
+import { nanoid } from "nanoid";
+import { useFretClickContext, useRenderedFrets } from "./MainContext";
 
 function Fretboard() {
+    const fretsToRender = useRenderedFrets();
+    const frets = fretsToRender.map((string) => {
+        return (
+            <ul key={nanoid()}>
+                {string.map((fretNote) => {
+                    return <Fret key={nanoid()} note={fretNote} />;
+                })}
+            </ul>
+        );
+    });
+
+    return <main>{frets}</main>;
 }
+
 export default Fretboard;
 
 function Fret({ note }: { note: string }) {
