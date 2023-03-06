d9ada91d4081b623070acc81b3c3e51be996b457
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-06 17:41:40 +0800

    Feat: add Fret component

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,3 +1,16 @@
 function Fretboard(){}
+import { useFretClickContext } from "./MainContext";
 
 export default Fretboard
+
+function Fret({ note }: { note: string }) {
+    const handleFretClick = useFretClickContext();
+    return (
+        <button
+            className='border border-zinc-200 w-8 h-8'
+            onClick={() => handleFretClick(note)}
+        >
+            {note}
+        </button>
+    );
+}
