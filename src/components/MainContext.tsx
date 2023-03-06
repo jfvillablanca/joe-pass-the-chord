9627d392986e68e1215277ac5b042baf49972dea
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-06 14:30:22 +0800

    Feat: provide "tuning" context to children

--- src/components/MainContext.tsx
+++ src/components/MainContext.tsx
@@ -1,18 +1,27 @@
 import { createContext, useContext } from "react";
 
 const FretClickContext = createContext<(note: string) => void>(() => {});
+const TuningContext = createContext<string[]>([]);
+
 export function useFretClickContext() {
     return useContext(FretClickContext);
 }
 
+export function useTuningContext() {
+    return useContext(TuningContext);
+}
+
 export function MainContext({ children }: { children: React.ReactNode }) {
+    const tuning = ["E", "A", "D", "G", "B", "E"];
     const handleFretClick = (note: string) => {
         console.log(note);
     };
 
     return (
-        <FretClickContext.Provider value={handleFretClick}>
-            {children}
-        </FretClickContext.Provider>
+        <TuningContext.Provider value={tuning}>
+            <FretClickContext.Provider value={handleFretClick}>
+                {children}
+            </FretClickContext.Provider>
+        </TuningContext.Provider>
     );
 }
