c16e9e17eb4c1e7b0afe43f286ec65ce86f63d53
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-06 17:36:24 +0800

    Feat: create context for renderedFrets state

--- src/components/MainContext.tsx
+++ src/components/MainContext.tsx
@@ -4,6 +4,7 @@ import { transpose } from "@tonaljs/note";
 
 const FretClickContext = createContext<(note: string) => void>(() => {});
 const TuningContext = createContext<string[]>([]);
+const RenderedFrets = createContext<string[][]>([]);
 
 export function useFretClickContext() {
     return useContext(FretClickContext);
@@ -11,6 +12,10 @@ export function useFretClickContext() {
 
 export function useTuningContext() {
     return useContext(TuningContext);
+}
+
+export function useRenderedFrets() {
+    return useContext(RenderedFrets);
 }
 
 export function MainContext({ children }: { children: React.ReactNode }) {
@@ -30,15 +35,19 @@ export function MainContext({ children }: { children: React.ReactNode }) {
     const tuning = ["E", "A", "D", "G", "B", "E"];
     const noOfFrets = 5;
     const [lowestRenderedFretNum, setLowestRenderedFretNum] = useState(1);
+    const [renderedFrets, setRenderedFrets] = useState(computeRenderedFrets());
+
     const handleFretClick = (note: string) => {
         console.log(note);
     };
 
     return (
         <TuningContext.Provider value={tuning}>
-            <FretClickContext.Provider value={handleFretClick}>
-                {children}
-            </FretClickContext.Provider>
+            <RenderedFrets.Provider value={renderedFrets}>
+                <FretClickContext.Provider value={handleFretClick}>
+                    {children}
+                </FretClickContext.Provider>
+            </RenderedFrets.Provider>
         </TuningContext.Provider>
     );
 }
