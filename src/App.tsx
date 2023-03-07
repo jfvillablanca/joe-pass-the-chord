72b6a1a81df697f3b141afb7707e1ba9495795f8
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-07 18:23:23 +0800

    Feat: import components and context to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import ChordDisplay from "./components/ChordDisplay";
+import Fretboard from "./components/Fretboard";
+import FretScroll from "./components/FretScroll";
+import { MainContext } from "./components/MainContext";
+
 function App() {
     return (
         <MainContext>
             <div className='App h-screen grid place-items-center'>
+                <FretScroll>
+                    <Fretboard />
+                </FretScroll>
+                <ChordDisplay />
             </div>
         </MainContext>
     );
