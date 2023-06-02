fcf20eb2f51bd346d0c5a4cb8dc407c0fc6ba1f7
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-03 04:13:14 +0800

    Refactor: display clean grid of chord inference

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -6,13 +6,33 @@ function Display() {
     const { tuning } = useFretboardContext();
     const { chordNames } = computeFingeredNotes(strings, tuning);
 
-    return (
-        <div className='alert alert-info'>
-            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' className='stroke-current shrink-0 w-6 h-6' > {" "} <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' ></path>{" "} </svg>
-            {chordNames.map((chord, key) => (
-                <span key={key}>{chord}</span>
-            ))}
+    const uniqueChordNames = [...new Set(chordNames.flat())];
+    const chordElements = uniqueChordNames.map((chord, key) => (
+        <div
+            className='bg-neutral text-neutral-content rounded-lg p-4 m-2 text-xl'
+            key={key}
+        >
+            {chord}
         </div>
+    ));
+
+    return (
+        <>
+            {chordElements.length === 0 ? (
+                <p className='font-bold text-xl uppercase'>
+                    No inferred chords
+                </p>
+            ) : (
+                <>
+                    <p className='font-bold text-xl uppercase'>
+                        Inferred chords:
+                    </p>
+                    <div className='grid grid-cols-3 auto-cols-min'>
+                        {chordElements}
+                    </div>
+                </>
+            )}
+        </>
     );
 }
 
